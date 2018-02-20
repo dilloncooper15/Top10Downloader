04ce9bb449a3814ca9b873a6a6c99baf176bc9b4
Dillon Cooper <dilloncooper@Dillons-MacBook-Pro.local>
2018-02-20 09:49:09 -0500

    Added Adapter for Recycler View that accepts the top 25 downloads from the App Store

--- app/src/main/java/com/example/dilloncooper/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/dilloncooper/top10downloader/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.dilloncooper.top10downloader
 
+import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
+import kotlin.properties.Delegates
 
 class FeedEntry {
     var name : String = ""
@@ -32,21 +37,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.d(TAG, "onCreate called")
-        val downloadData = DownloadData()
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        val downloadData = DownloadData(this, xmlListView)
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml")
         Log.d(TAG, "onCreate: done")
     }
 
     // Companion Object = Kotlin's version of a Java Static Class
-    companion object {
-        private class DownloadData : AsyncTask<String, Void, String>() {
+    companion object {  // Created companion object to avoid memory leaks
+        private class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
+
+            var propContext : Context by Delegates.notNull()
+            var propListView : ListView by Delegates.notNull()
+
+            init {
+                propContext = context
+                propListView = listView
+            }
 
             override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
 //                Log.d(TAG, "onPostExecute: parameter is $result")
                 val parseApplications = ParseApplications()
                 parseApplications.parse(result)
+
+                val arrayAdapter = ArrayAdapter<FeedEntry>(propContext, R.layout.list_item, parseApplications.applications)
+                propListView.adapter = arrayAdapter
             }
 
             override fun doInBackground(vararg url: String?): String {
