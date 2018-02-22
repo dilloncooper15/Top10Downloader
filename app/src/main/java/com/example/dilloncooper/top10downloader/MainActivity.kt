211709bb44a054520b1f4801587c65cda65bc37a
http://192.168.0.71:8080/tfs/DefaultCollection/MobileApplications/_git/MobileEstimator/dillonc <dillon.cooper@twomen.com>
2018-02-22 14:03:59 -0500

    Added caching of URL so the user cannot continuously select the currently selected URL, added a refresh feature, added functionality so if the user rotates the device, it restores their last state and does not download a new instance

--- app/src/main/java/com/example/dilloncooper/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/dilloncooper/top10downloader/MainActivity.kt
@@ -7,21 +7,22 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.accessibility.AccessibilityNodeInfo.ACTION_CLICK
+import android.view.accessibility.AccessibilityNodeInfo.ACTION_LONG_CLICK
 import android.widget.ListView
+import com.example.dilloncooper.top10downloader.R.id.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.URL
 import kotlin.properties.Delegates
 
 
-private const val STATE_FEEDLIMIT = ""
-private const val STATE_SAVEURL =
-
 class FeedEntry {
-    var name : String = ""
-    var artist : String = ""
-    var releaseDate : String = ""
-    var summary : String = ""
-    var imageURL : String = ""
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
 
     override fun toString(): String {  // Override toString so the Recycler View does not display the full class name and hashcode of each instance of the FeedEntry class.
         return """
@@ -37,36 +38,59 @@ class FeedEntry {
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
-//    private val downloadData by lazy { DownloadData(this, xmlListView) }  // Have to add by lazy, since none of the widgets exist until after the call to setContentView in the onCreate Method.
+    //    private val downloadData by lazy { DownloadData(this, xmlListView) }  // Have to add by lazy, since none of the widgets exist until after the call to setContentView in the onCreate Method.
     private var downloadData: DownloadData? = null  //Have to change downloadData to allow nulls since we will be referring to Async multiple times via the user selecting an option from the menu.
     // NOTE: With a lazy function, initialization does not occur until the first time we need to use the DownloadData object.
 
     private var feedUrl: String = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml"
     private var feedLimit = 10  //default to Top 10
 
+    private var feedCachedUrl = "INVALIDATED"
+    private val STATE_URL = "feedUrl"
+    private val STATE_LIMIT = "feedLimit"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (savedInstanceState != null) {  //Since onRestore occurs after onCreate in the Android Lifecycle, we have to put the restoration in onCreate
+
+            val saveUrls = savedInstanceState.getString(STATE_URL)
+            feedUrl = saveUrls
+
+            val saveFeed = savedInstanceState.getInt(STATE_LIMIT)
+            feedLimit = saveFeed
+        }
+
         downloadUrl(feedUrl.format(feedLimit))  //Formats feedUrl to substitute the value specified for "feedLimit" into the url in "feedUrl" for the value of "%d"
-        Log.d(TAG,"onCreate: done")
+        Log.d(TAG, "onCreate: done")
     }
 
     private fun downloadUrl(feedUrl: String) {
-        Log.d(TAG, "downloadURL starting AsyncTask")
-        downloadData = DownloadData(this, xmlListView)
-        downloadData?.execute(feedUrl)
-        Log.d(TAG, "downloadUrl done")
+        if (feedUrl != feedCachedUrl) {  //Checks and prevents the user from re-downloading the same URL if they are already viewing that URL
+            Log.d(TAG, "downloadURL starting AsyncTask")
+            downloadData = DownloadData(this, xmlListView)
+            downloadData?.execute(feedUrl)
+            Log.d(TAG, "downloadUrl done")
+            Log.d(TAG, "downloadURL1 $feedCachedUrl")
+            feedCachedUrl = feedUrl  // Update feedCachedUrl to contain the currently accessed URL
+            Log.d(TAG, "downloadURL2 $feedCachedUrl")
+        } else {
+            Log.d(TAG, "downloadUrl - URL not changed")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {  //Gives us the ellipses within the menu and displays the three options depicted in feeds_menu.xml
         menuInflater.inflate(R.menu.feeds_menu, menu)
 
         if (feedLimit == 10) {
+            Log.d(TAG, "$feedLimit")
             menu?.findItem(R.id.mnu10)?.isChecked = true
+        } else {
             menu?.findItem(R.id.mnu25)?.isChecked = true
         }
-
+        Log.d(TAG, "$feedLimit")
         return true
     }
 
@@ -79,6 +103,7 @@ class MainActivity : AppCompatActivity() {
                 feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml"
             R.id.mnuSongs ->
                 feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml"
+
             R.id.mnu10, R.id.mnu25 -> {
                 if (!item.isChecked) { //If item is not currently checked
                     item.isChecked = true  //Set isChecked to now true
@@ -89,13 +114,26 @@ class MainActivity : AppCompatActivity() {
                     return false
                 }
             }
+            R.id.mnuRefresh ->
+                feedCachedUrl = "INVALIDATED"
+
             else ->
                 return super.onOptionsItemSelected(item)
         }
 
         downloadUrl(feedUrl.format(feedLimit))
+        Log.d(TAG, "$feedLimit")
+
         return true
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString(STATE_URL, feedUrl)
+        outState.putInt(STATE_LIMIT, feedLimit)
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -107,8 +145,8 @@ class MainActivity : AppCompatActivity() {
         private class DownloadData(context: Context, listView: ListView) : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
 
-            var propContext : Context by Delegates.notNull()
-            var propListView : ListView by Delegates.notNull()
+            var propContext: Context by Delegates.notNull()
+            var propListView: ListView by Delegates.notNull()
 
             init {
                 propContext = context
@@ -132,18 +170,9 @@ class MainActivity : AppCompatActivity() {
             }
 
             private fun downloadXML(urlPath: String?): String {
-               return URL(urlPath).readText()
+                return URL(urlPath).readText()
             }
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        var saveUrlLimit = outState.putInt(feedUrl, feedLimit)
-//        cacheDir.
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
-    }
 }
